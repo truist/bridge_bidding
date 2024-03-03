42015b608664af23c4269e2c88bcea7c87d7e0c0
Nathan Arthur <narthur-git@rainskit.com>
2024-03-03 00:05:42 -0500

    Remove the png; it didn't work like I hoped

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -e
 # depends on https://github.com/mermaid-js/mermaid-cli
 
 mmdc -c config.js -i bidding.mmd -o bidding.svg
-mmdc -c config.js -w 3840 -s 2 -i bidding.mmd -o bidding.png
+# mmdc -c config.js -w 3840 -s 2 -i bidding.mmd -o bidding.png
 
