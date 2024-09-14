6709cb3036b5e0063f99e9004a09787d271e122c
Nathan Arthur <narthur-git@rainskit.com>
2024-09-14 19:27:31 -0400

    Checkpoint on new transfers.mmd

--- build.sh
+++ build.sh
@@ -4,6 +4,34 @@ set -e
 
 # depends on https://github.com/mermaid-js/mermaid-cli
 
-mmdc -c config.js -i bidding.mmd -o bidding.svg
+OPEN=0
+
+generate() {
+	file="$1"
+
+	base="$(basename "$file" .mmd)"
+	echo "$base.mmd -> $base.svg"
+	mmdc -c config.js -i "$base.mmd" -o "$base.svg"
+
+	if [[ "$OPEN" -eq 1 ]]; then
+		open "$base.svg"
+	fi
+}
+
+if [[ "$1" == "-o" ]]; then
+	OPEN=1
+	shift
+fi
+
+if [[ $# -gt 0 ]]; then
+	generate "$1"
+else
+	for mmd in *.mmd ; do
+		generate "$mmd"
+	done
+fi
+
+
+# mmdc -c config.js -i bidding.mmd -o bidding.svg
 # mmdc -c config.js -w 3840 -s 2 -i bidding.mmd -o bidding.png
 
